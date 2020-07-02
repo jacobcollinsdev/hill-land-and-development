e9adefae7d28fca0c59e72f007e42cc0a0000a3f
Jacob Collins <jacobc1596@gmail.com>
2020-07-02 18:00:14 +0100

    Added GSAP animations

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,98 @@ scrollDown.click(()=>{
         scrollTop: $('#buying-selling').offset().top
     }, 200);
 })
+
+//Animations
+
+gsap.from(".welcome-statement",{
+    opacity:0,
+    duration:1,
+    scaleX:1.2,
+    scaleY:1.2
+})
+
+gsap.from(".nav-link",{
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+
+let tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.buying-selling',
+        start: "center bottom",
+        toggleActions: "play none none reverse",
+        ease: "power0"
+    }
+})
+
+tl.from(".buying",{
+    x:-200,
+    opacity:0,
+    duration:1
+}).from(".selling",{
+    x:200,
+    opacity:0,
+    duration:1
+}, "-=1")
+
+let tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".why-us",
+        start:"center bottom",
+        toggleActions: "play none none reverse",
+    }
+})
+
+tl2.from(".anim1",{
+    x:-200,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+
+let tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".projects",
+        start:"center-=100 bottom",
+        toggleActions: "play none none reverse",
+        ease:"power2.easeOut"
+
+    }
+})
+
+tl3.from(".anim2",{
+    x:-200,
+    opacity:0,
+    duration:0.5
+}).from(".anim3",{
+    x:200,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+}, "-=0.5")
+
+tl4 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".contact",
+        start:"center bottom",
+        toggleActions: "play none none reverse",
+        ease:"power2.easeOut"
+    }
+})
+
+tl4.from(".anim4",{
+    x:-200,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+}).from(".anim5",{
+    x:200,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+}, "-=0.5")
+
+
+
+
